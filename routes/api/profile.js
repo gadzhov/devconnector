559ca1eb91d8f9b250daf4027af8cf9bd56db754
vladimir.gadzhov <vladimir.gadzhov@gmail.com>
2019-11-29 00:19:13 +0200

    Delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -167,4 +167,64 @@ router.delete('/', auth, async (req, resp) => {
   }
 });
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, resp) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      resp.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      resp.json(profile);
+    } catch (err) {
+      console.log(err.message);
+      resp.status(500).send('Server error');
+    }
+  }
+);
+
 module.exports = router;
