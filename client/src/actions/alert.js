adf84d13b9092d4b2bfe1e6dfad059908e2cc352
vladimir.gadzhov <vladimir.gadzhov@gmail.com>
2019-12-02 23:30:23 +0200

    Alert component action call

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import uuid from 'uuid';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeOut = 5000) => dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
@@ -11,4 +11,13 @@ export const setAlert = (msg, alertType) => dispatch => {
       id
     }
   });
+
+  setTimeout(
+    () =>
+      dispatch({
+        type: REMOVE_ALERT,
+        payload: id
+      }),
+    timeOut
+  );
 };
