e3e2ad4bdcbe19d095404f2ad4f5281edfdb3035
vladimir.gadzhov <vladimir.gadzhov@gmail.com>
2019-12-08 12:58:41 +0200

    Load user set auth token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,31 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 import { setAlert } from './alert';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from './types';
+import setAtuhToken from '../utils/setAuthToken';
+
+// Load user
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAtuhToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get('/api/auth');
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 // Register user
 export const register = ({ name, email, password }) => async dispatch => {
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,9 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -19,12 +24,20 @@ export default function(state = initialState, action) {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false
+      };
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
       };
     default:
       return state;
