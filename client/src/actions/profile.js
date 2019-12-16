baa4d189cca39a8f75350ee5e6cff6a18a7cc630
vladimir.gadzhov <vladimir.gadzhov@gmail.com>
2019-12-16 15:26:06 +0200

    Profile reducer get current profile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+
+// Get current users profile
+export const getCurrentProfile = () => async dispatch => {
+  try {
+    const resp = await axios.get('/api/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: resp.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
