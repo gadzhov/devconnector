2544120aae4075e6d66eaedbee406e794f66a0ed
vladimir.gadzhov <vladimir.gadzhov@gmail.com>
2019-12-18 17:22:46 +0200

    Adding comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
-  GET_POST
+  GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -53,6 +55,26 @@ export default function(state = initialState, action) {
         posts: state.posts.map(post =>
           post._id === payload.postId ? { ...post, likes: payload.likes } : post
         ),
+        loading: false
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: payload
+        },
+        loading: false
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.posts.comment.filter(
+            comment => comment._id !== payload
+          )
+        },
         loading: false
       };
 
